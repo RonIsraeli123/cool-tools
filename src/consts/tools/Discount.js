87858cac636274615b86b797577c1dae1ecb1bbe
stamdvarim1000 <stamdvarim1000@gmail.com>
2022-03-16 01:19:47 +0200

    fix typo

--- src/consts/tools/Discount.js
+++ src/consts/tools/Discount.js
@@ -3,6 +3,6 @@ import discountImage from '../../images/discount.jpg';
 export const HEADER = 'Discount header';
 
 export const TEXT =
-  'With this tool you can calculate how mach you needs to pay after the discount';
+  'With this tool you can calculate how much you need to pay after the discount';
 
 export const IMAGE = discountImage;
